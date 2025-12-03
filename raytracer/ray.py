28b2bcdd3ad12ea7dfb0ebd8c33b185944f21448
Yael Sarne <sarne.yael@gmail.com>
2025-12-03 20:05:52 +0200

    added find_ray_closest_intersection

--- raytracer/ray.py
+++ raytracer/ray.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Ray:
     def __init__(self, camera_point, pixel_point):
@@ -7,3 +8,21 @@ class Ray:
 
         self.V = self.pixel_point - self.camera_point
         self.V = self.V / np.linalg.norm(self.V)
+    
+    def find_ray_closest_intersection(self, objects):
+        t_min = math.inf
+        closest_hit_point = np.array([math.inf,math.inf,math.inf], dtype=float)
+        closest_obj = None
+        for obj in objects:
+            result = obj.find_intersection(self)
+            if result is None:
+                continue
+            hit_point, t = result
+            if t < t_min:
+                t_min = t
+                closest_hit_point = hit_point
+                closest_obj = obj
+        if closest_obj is None:
+            return None, None, None
+        return t_min, closest_hit_point, closest_obj
+        
